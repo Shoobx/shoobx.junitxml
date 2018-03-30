dba87fc6d7f6d83540ede334fe5048b8744bfaa2
Justas Sadzevicius <justas@shoobx.com>
2018-03-30 16:45:25 +0300

    Back to development: 0.1.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shoobx.junitxml',
-    version='0.1.4',
+    version='0.1.5.dev0',
     description=
         'A zope.testrunner output formatter & feature to output JUnit XML.',
     long_description=(
