058f6abc9abd1eff78c8afb71c56c54ec2f1901a
Adam Groszer <adam.groszer@fmr.com>
2024-12-18 13:49:28 +0100

    Back to development: 0.3.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shoobx.junitxml',
-    version='0.3.0',
+    version='0.3.1.dev0',
     description=
         'A zope.testrunner output formatter & feature to output JUnit XML.',
     long_description=(
